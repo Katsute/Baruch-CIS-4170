ebdaa69f3fffacfaa0e9858e2ca7b42164f39463
Katsute <58778985+Katsute@users.noreply.github.com>
2021-02-08 21:03:42 -0500

    m

---
+++ classwork/02_08_2021/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.plot([10, 30, 20])
+plt.show()
+
+
+# In[ ]:
+
+
+
+
